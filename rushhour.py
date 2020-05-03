2105e73c43bb6bf4577c10b5aaff8ffa314b5658
Jacie Mae <jcmoninghoff@gmail.com>
2020-05-02 18:03:41 -0700

    GenerateChildren working

--- rushhour.py
+++ rushhour.py
@@ -2,6 +2,12 @@ from queue import PriorityQueue
 from copy import deepcopy
 
 
+def testinput():
+    #inputBoard(["--B---","--B---","XXB---","--AA--", "------","------"])
+    #inputBoard(["---O--","---O--","XX-O--","PQQQ--", "P-----","P-----"])
+    #inputBoard(["OOOP--","--AP--","XXAP--","Q-----", "QGGCCD","Q----D"])
+    inputBoard(["--OPPP","--O--A","XXO--A","-CC--Q", "-----Q","--RRRQ"])
+
 class Car:
         def __init__(self, start, char, orient, leng):
             self.char = char
@@ -9,20 +15,15 @@ class Car:
             self.start = start
             self.len = leng
 
-        def move(self, dir):
-            if dir == 'F':
-                if self.start[self.orient] < 5:
-                    self.start[0] += 1
-                    return [True, self]
-                else:
-                    return False
-            elif dir == 'B':
-                if self.start[self.orient] > 0:
-                    self.start[0] += -1
-                    return [True, self]
-                else:
-                    return False
         
+        def print(self):
+            print(
+                'Name: ', self.char,
+                '\nOrientation: ', self.orient,
+                '\nStart: ', self.start,
+                '\nLength: ', self.len
+            )
+
         def sizeup(self):
             self.len += 1
 
@@ -54,11 +55,19 @@ class Board:
                         cars[chars.index(inBoard[x][y])].sizeup()
 
         self.cars = cars
+        self.board = self.tochararray()
+        print(
+            'board: ', self.board,
+            'board[0]: ', self.board[0],
+            'board[0][0]: ', self.board[0][0],
+            'board[0][2]: ', self.board[0][2]
+            
+        )
 
         
 
     def tochararray(self):
-        arr = [[0 for x in range(6)] for y in range(6)]
+        arr = [['-' for x in range(6)] for y in range(6)]
         #for char in self.chars:
         
         for car in self.cars:
@@ -70,14 +79,66 @@ class Board:
                     arr[x+i][y] = car.char
                 elif car.orient == 1:
                     arr[x][y+i] = car.char
-        
-        for row in arr:
-            print(row)
-        print('\n\n')
         return arr
 
-    def boardcopy(self):
-        return self
+    def printboard(self):
+        for row in self.board:
+            print(row)
+        print('\n')
+
+    def movecarforward(self, carIndex):
+        print('beginMove f')
+        print(self.board)
+        car = self.cars[carIndex]
+        x = car.start[0]
+        y = car.start[1]
+        orient = car.orient
+        forwardSpace = []
+        if orient == 0:
+            forwardSpace = self.board[x + car.len][y]
+        elif orient == 1:
+            forwardSpace = self.board[x][y + car.len]
+
+        print(
+            '(x,y): (', x, ',', y,')',
+            '\norient: ', orient,
+            '\nforwardSpace: ', forwardSpace,
+            '\ncar.start[orient]: ', car.start[orient]
+        )
+
+        if car.start[orient] < 5 and forwardSpace == '-':
+            print('before move: ', car.start[car.orient])
+            car.start[orient] += 1
+            print('after move: ', car.start[car.orient])
+            self.board = self.tochararray()
+            
+        #    return [True, self]
+        #else:
+        #    return False
+        
+        
+    def movecarbackward(self, carIndex):
+        print('beginMove b')
+        print(self.board)
+        car = self.cars[carIndex]
+        x = car.start[0]
+        y = car.start[1]
+        orient = car.orient
+        backSpace = []
+        if orient == 0:
+            backSpace = self.board[x - 1][y]
+        elif orient == 1:
+            backSpace = self.board[x][y - 1]
+
+
+        
+        print('move b: ')
+        if car.start[orient] > 0 and backSpace == '-':
+            print('before move: ', car.start[car.orient])
+            car.start[orient] += -1
+            print('after move: ', car.start[car.orient])
+            self.board = self.tochararray()
+
 
     
     def __eq__(self, other):
@@ -99,40 +160,57 @@ def inputBoard(inBoard):
     print('Original:')
     board.tochararray()
     print('NewBoards:')
-    newBoards = generateChildren(board)
-    newBoards[0].tochararray()
+    newBoards = generateChildren(board)    
 
 
 def generateChildren(inBoard):
     newBoards = []
     for car in inBoard.cars:
         board = generateChild(inBoard, car, 'F')
-        if board != None and board != []:
+        if board != None and board != inBoard:
             newBoards += [board]
-
+        
+        
         board = generateChild(inBoard, car, 'B')
-        if board != None and board != []:
+        if board != None and board != inBoard:
             newBoards += [board]
 
+    print('original board: ')
+    inBoard.printboard()
+
+    print('new boards: ', len(newBoards))
+    
+    for item in newBoards:
+        item.tochararray()
+        item.printboard()
     return newBoards
 
     #TODO: Implement way to save each new board, without having to deepcopy
     #Also, calculate A* dist in here to return as tuple
 
 def generateChild(board, car, dir):
-
+    
+    carIndex = board.cars.index(car)
 
     #TODO: Check to see if there is another car before making moves
     if dir == 'F' and car.start[car.orient] + car.len < 5:
-        localState = board.boardcopy()
         
-        #whichCar = localState.cars.index(car)
-        localState.cars[0].move(dir)
+        localState = deepcopy(board)
+        print('localstate before move: ')
+        localState.printboard()
+        localState.movecarforward(carIndex)
+        localState.cars[carIndex].print()
+        print('localstate after move: ')
+        localState.printboard()
         return localState
     if dir == 'B' and car.start[car.orient] > 0:
-        localState = board.boardcopy()
-        #whichCar = localState.cars.index(car)
-        localState.cars[0].move(dir)
+        localState = deepcopy(board)
+        print('localstate before move: ')
+        localState.printboard()
+        localState.movecarbackward(carIndex)
+        localState.cars[carIndex].print()
+        print('localstate after move: ')
+        localState.printboard()
         return localState
         
 
@@ -152,8 +230,9 @@ def rushhour(heuristic, startBoard):
 def astar(startBoard):
     open = PriorityQueue()
 
-    dist = findadist(startBoard)
-    open.put((dist, startBoard, []))
+    open.put(startBoard)
+    cameFrom[startBoard] = None
+    dist[startBoard] = findadist(startBoard)
 
     #while not open.empty():
     #    currBoard = open.get()
